cdc08ce0b826d4079c4895404258ab620414e954
Ritik Pathak <ritik@vrozart.com>
2025-06-29 13:43:17 +0530

    basic flow of the fast api

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 
+from typing import Optional
 from fastapi import FastAPI
-from uvicorn import run
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    title:str
+    author:str
+    description:Optional[str]=None
+    rating:int
 
 app = FastAPI()
 
@@ -12,3 +19,26 @@ def read_root():
 @app.get("/about")
 def get_about():
     return {"message": "About page"}
+
+# Path Parameters
+@app.get("/greet/{name}")
+async def greet(name:str) -> dict:
+    return {"message": f"Hello {name}"}
+
+# Query Parameters
+@app.get("/greet1/{name}")
+async def greet_name(name:str,age:Optional[int]=None) -> dict:
+    return {"message":f"Hello My name is {name} & the age is {age}"}
+
+
+@app.post("/create-book")
+async def create_book(book:Book)->dict:
+    new_book = {
+        "title":"The Great Gatsby",
+        "author":"John Doe",
+        "description":"This is a description",
+        "rating":11
+    }
+
+
+    return {"message":"Book created", "book":new_book}
